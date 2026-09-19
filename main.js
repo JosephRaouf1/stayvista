47fb67fd38caea9adffb20522b7f741868d1b064
Joseph Raouf <279377251+JosephRaouf1@users.noreply.github.com>
2026-09-19 09:42:59 +0200

    Update input date

--- main.js
+++ main.js
@@ -19,3 +19,46 @@ links.forEach(link => {
         };
     });
 });
+
+
+const dateInputs = document.querySelectorAll(".date_input");
+
+dateInputs.forEach(function (dateInput) {
+    const input = dateInput.querySelector("input");
+    const text = dateInput.querySelector("span");
+
+    input.addEventListener("change", function () {
+        if (this.value === "") {
+            text.textContent = text.dataset.default;
+        } else {
+            text.textContent = this.value;
+        }
+    });
+});
+
+const inputs = document.querySelectorAll(".date_input input");
+
+const checkIn = inputs[0];
+const checkOut = inputs[1];
+
+const today = new Date().toISOString().split("T")[0];
+
+checkIn.min = today;
+checkOut.min = today;
+
+checkIn.addEventListener("change", function () {
+    if (this.value === "") {
+        checkOut.min = today;
+        checkOut.max = "";
+    } else {
+        checkOut.min = this.value;
+    }
+});
+
+checkOut.addEventListener("change", function () {
+    if (this.value === "") {
+        checkIn.max = "";
+    } else {
+        checkIn.max = this.value;
+    }
+});
